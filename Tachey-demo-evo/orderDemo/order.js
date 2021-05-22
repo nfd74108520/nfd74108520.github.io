7a1dcab8992a9ea3f89d0bdfb4aec6723754591d
George <nfd74108520@gmail.com>
2021-05-22 13:51:08 +0800

    neworeder

--- Tachey-demo-evo/orderDemo/order.js
+++ Tachey-demo-evo/orderDemo/order.js
@@ -1,9 +1,22 @@
-let changeBtn = document.querySelectorAll(".change-btn");
+let changeBtn = document
+  .querySelector(".checked-btn")
+  .querySelectorAll("button");
 
 changeBtn.forEach((item) => {
-    item.addEventListener("click", changeIndex)
+  item.addEventListener("click", changeIndex);
 });
 
-function changeIndex(){
-    console.log(event.target)
+function changeIndex(e) {
+  changeBtn.forEach((item) => {
+    if (item.classList.contains(e.target.id)) {
+      item.classList.add("change-btn-checked");
+      item.classList.remove("change-btn");
+      let emptyText = document.getElementById("emptyText");
+
+      emptyText.innerText = item.innerText;
+    } else {
+      item.classList.remove("change-btn-checked");
+      item.classList.add("change-btn");
+    }
+  });
 }
