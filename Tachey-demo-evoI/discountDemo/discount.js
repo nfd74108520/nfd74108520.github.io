1f11668d3a9cbb4b703d4bd69b63488296bb85c4
George <nfd74108520@gmail.com>
2021-05-22 17:22:35 +0800

    evoiii

--- Tachey-demo-evoI/discountDemo/discount.js
+++ Tachey-demo-evoI/discountDemo/discount.js
@@ -9,12 +9,15 @@ changeBtn.forEach((item) => {
 function changeIndex(e) {
   changeBtn.forEach((item) => {
     if (item.classList.contains(e.target.id)) {
+      item.parentNode.classList.add("change-btn-checked-border");
       item.classList.add("change-btn-checked");
       item.classList.remove("change-btn");
       let emptyText = document.getElementById("emptyText");
 
       emptyText.innerText = item.innerText;
+      console.dir(item.parentNode.classList);
     } else {
+      item.parentNode.classList.remove("change-btn-checked-border");
       item.classList.remove("change-btn-checked");
       item.classList.add("change-btn");
     }
